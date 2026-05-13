40c2e16070b68645dd13efa061628f824f1265bb
Avis <aviscristiano10@gmail.com>
2026-05-13 20:04:23 +0530

    Promotion screen Ui fix

--- Src/Screens/PromotionsMain.tsx
+++ Src/Screens/PromotionsMain.tsx
@@ -1,37 +1,54 @@
-import { View, StatusBar } from "react-native";
+import { View, StyleSheet } from "react-native";
 import React from "react";
-import { SafeAreaView } from "react-native-safe-area-context";
 import { useRoute } from "@react-navigation/native";
 import NewAppHeader from "../Components/NewAppHeader";
+import { Image } from "expo-image";
 import { COLORS } from "../Constants/Theme";
-import {Image} from 'expo-image';
 
-const PromotionsMain = ({navigation}: any) => {
+const PromotionsMain = ({ navigation }: any) => {
   const route = useRoute<any>();
-  const { bannerImage, bannerType } = route.params;  
+
+  const { bannerImage, bannerType } = route.params;
+
   const handleBackPress = () => {
     navigation.goBack();
   };
 
   return (
-    <SafeAreaView style={{ 
-        flex: 1,
-        backgroundColor: COLORS.primary,
-     }}>
-        {/* <StatusBar backgroundColor={COLORS.primary} barStyle="light-content" /> */}
-        <NewAppHeader
+    <View style={styles.container}>
+      <NewAppHeader
         leftIconPress={handleBackPress}
         centerText={bannerType}
-        // rightIcon={require('../../assets/wallet-icon.webp')}
       />
+
+      <View style={styles.imageContainer}>
         <Image
           source={bannerImage}
-          style={{ width: "90%", height: '90%', marginHorizontal: 20 }}
-          contentFit= "cover"
+          style={styles.image}
+          contentFit="contain"
         />
-
-    </SafeAreaView>
+      </View>
+    </View>
   );
 };
 
 export default PromotionsMain;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.primary,
+  },
+
+  imageContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 12,
+  },
+
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
